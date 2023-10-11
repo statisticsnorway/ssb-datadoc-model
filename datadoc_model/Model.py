bd88fd81952c9d4a72530a2e266dfc076f0550d8
Johnny Niklasson <jnk@ssb.no>
2023-10-11 13:29:06 +0200

    updated to pydantic v2.4.2 (#14)

--- datadoc_model/Model.py
+++ datadoc_model/Model.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 from typing import List, Optional
-from pydantic import constr, conint
+from pydantic import Field, StringConstraints
 import logging
 from uuid import UUID
 
@@ -8,6 +8,7 @@ from datadoc_model import Enums
 from datadoc_model.BaseModel import DataDocBaseModel
 from datadoc_model.LanguageStrings import LanguageStrings
 from datadoc_model.LanguageStringsEnum import LanguageStringsEnum
+from typing_extensions import Annotated
 
 MODEL_VERSION = "1.0.0"
 
@@ -21,64 +22,87 @@ class DataDocDataSet(DataDocBaseModel):
     """DataDoc data set. See documentation https://statistics-norway.atlassian.net/l/c/NgjE7yj1"""
 
     short_name: Optional[
-        constr(min_length=1, max_length=63, regex=ALPHANUMERIC_HYPHEN_UNDERSCORE)
-    ]
-    assessment: Optional[Enums.Assessment]
+        Annotated[
+            str,
+            StringConstraints(
+                min_length=1, max_length=63, pattern=ALPHANUMERIC_HYPHEN_UNDERSCORE
+            ),
+        ]
+    ] = None
+    assessment: Optional[Enums.Assessment] = None
     dataset_status: Optional[Enums.DatasetStatus] = Enums.DatasetStatus.DRAFT
-    dataset_state: Optional[Enums.DatasetState]
-    name: Optional[LanguageStrings]
-    data_source: Optional[LanguageStrings]
-    population_description: Optional[LanguageStrings]
-    version: Optional[str]
-    version_description: Optional[str]
-    unit_type: Optional[Enums.UnitType]
-    temporality_type: Optional[Enums.TemporalityType]
-    description: Optional[LanguageStrings]
-    subject_field: Optional[LanguageStrings]
-    keyword: Optional[List[str]]
-    spatial_coverage_description: Optional[LanguageStrings]
-    id: Optional[UUID]
-    owner: Optional[LanguageStrings]
-    data_source_path: Optional[str]
-    metadata_created_date: Optional[datetime]
-    metadata_created_by: Optional[str]
-    metadata_last_updated_date: Optional[datetime]
-    metadata_last_updated_by: Optional[str]
-    contains_data_from: Optional[date]
-    contains_data_until: Optional[date]
+    dataset_state: Optional[Enums.DatasetState] = None
+    name: Optional[LanguageStrings] = None
+    data_source: Optional[LanguageStrings] = None
+    population_description: Optional[LanguageStrings] = None
+    version: Optional[str] = None
+    version_description: Optional[str] = None
+    unit_type: Optional[Enums.UnitType] = None
+    temporality_type: Optional[Enums.TemporalityType] = None
+    description: Optional[LanguageStrings] = None
+    subject_field: Optional[LanguageStrings] = None
+    keyword: Optional[List[str]] = None
+    spatial_coverage_description: Optional[LanguageStrings] = None
+    id: Optional[UUID] = None
+    owner: Optional[LanguageStrings] = None
+    data_source_path: Optional[str] = None
+    metadata_created_date: Optional[datetime] = None
+    metadata_created_by: Optional[str] = None
+    metadata_last_updated_date: Optional[datetime] = None
+    metadata_last_updated_by: Optional[str] = None
+    contains_data_from: Optional[date] = None
+    contains_data_until: Optional[date] = None
 
 
 class DataDocVariable(DataDocBaseModel):
     """DataDoc instance variable. See documentation https://statistics-norway.atlassian.net/l/c/goyNhUPP"""
 
     short_name: Optional[
-        constr(min_length=1, max_length=63, regex=ALPHANUMERIC_HYPHEN_UNDERSCORE)
-    ]
-    name: Optional[LanguageStrings]
-    data_type: Optional[Enums.Datatype]
-    variable_role: Optional[Enums.VariableRole]
-    definition_uri: Optional[constr(min_length=1, max_length=63, regex=URL_FORMAT)]
-    direct_person_identifying: Optional[bool]
-    data_source: Optional[LanguageStrings]
-    population_description: Optional[LanguageStrings]
-    comment: Optional[LanguageStrings]
-    temporality_type: Optional[Enums.TemporalityType]
+        Annotated[
+            str,
+            StringConstraints(
+                min_length=1, max_length=63, pattern=ALPHANUMERIC_HYPHEN_UNDERSCORE
+            ),
+        ]
+    ] = None
+    name: Optional[LanguageStrings] = None
+    data_type: Optional[Enums.Datatype] = None
+    variable_role: Optional[Enums.VariableRole] = None
+    definition_uri: Optional[
+        Annotated[
+            str, StringConstraints(min_length=1, max_length=63, pattern=URL_FORMAT)
+        ]
+    ] = None
+    direct_person_identifying: Optional[bool] = None
+    data_source: Optional[LanguageStrings] = None
+    population_description: Optional[LanguageStrings] = None
+    comment: Optional[LanguageStrings] = None
+    temporality_type: Optional[Enums.TemporalityType] = None
     # TODO: measurement_unit implemented as string. In the future this should be implemente as a class? See https://www.ssb.no/klass/klassifikasjoner/303/koder
-    measurement_unit: Optional[LanguageStrings]
-    format: Optional[str]
-    classification_uri: Optional[constr(min_length=1, max_length=63, regex=URL_FORMAT)]
-    sentinel_value_uri: Optional[constr(min_length=1, max_length=63, regex=URL_FORMAT)]
-    invalid_value_description: Optional[LanguageStrings]
-    id: Optional[constr(regex=URL_FORMAT)]
-    contains_data_from: Optional[date]
-    contains_data_until: Optional[date]
+    measurement_unit: Optional[LanguageStrings] = None
+    format: Optional[str] = None
+    classification_uri: Optional[
+        Annotated[
+            str, StringConstraints(min_length=1, max_length=63, pattern=URL_FORMAT)
+        ]
+    ] = None
+    sentinel_value_uri: Optional[
+        Annotated[
+            str, StringConstraints(min_length=1, max_length=63, pattern=URL_FORMAT)
+        ]
+    ] = None
+    invalid_value_description: Optional[LanguageStrings] = None
+    id: Optional[Annotated[str, StringConstraints(pattern=URL_FORMAT)]] = None
+    contains_data_from: Optional[date] = None
+    contains_data_until: Optional[date] = None
 
 
 class MetadataDocument(DataDocBaseModel):
     """Represents the data structure on file. Includes the dataset metadata from the user as
-    well as meta-metadata like the percentage of completed metadata fields and the document version"""
+    well as meta-metadata like the percentage of completed metadata fields and the document version
+    """
 
-    percentage_complete: conint(ge=0, le=100)
+    percentage_complete: Annotated[int, Field(ge=0, le=100)]
     document_version: str = MODEL_VERSION
     dataset: DataDocDataSet
     variables: List[DataDocVariable]
