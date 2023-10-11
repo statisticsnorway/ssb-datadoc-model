8d440283db0969f34e4ec2236230003ddd3fe550
ssb-jnk <jnk@ssb.no>
2023-10-11 11:17:09 +0200

    updated to pydantic v2.4.2

--- datadoc_model/BaseModel.py
+++ datadoc_model/BaseModel.py
@@ -1,11 +1,6 @@
-from pydantic import BaseModel
+from pydantic import ConfigDict, BaseModel
 
 
 class DataDocBaseModel(BaseModel):
     """Defines configuration which applies to all Models in this application"""
-
-    class Config:
-        # Runs validation when a field value is assigned, not just in the constructor
-        validate_assignment = True
-        # Write only the values of enums during serialization
-        use_enum_values = True
+    model_config = ConfigDict(validate_assignment=True, use_enum_values=True)
