f2afbdbea0785d37b63e80031e3a7c8ee13e81fe
mmwinther <mmw@ssb.no>
2024-01-12 09:07:53 +0100

    Remove datadoc_model_legacy

--- generated/python/datadoc_model_legacy/datadoc_model/BaseModel.py
+++
@@ -1,6 +0,0 @@
-from pydantic import ConfigDict, BaseModel
-
-
-class DataDocBaseModel(BaseModel):
-    """Defines configuration which applies to all Models in this application"""
-    model_config = ConfigDict(validate_assignment=True, use_enum_values=True)
