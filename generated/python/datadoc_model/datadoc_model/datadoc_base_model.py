7e67de907dd969df136599143f5f2f9295369c84
Miles Mason Winther <42948872+mmwinther@users.noreply.github.com>
2024-02-21 10:00:06 +0100

    Forbid extra fields

--- generated/python/datadoc_model/datadoc_model/datadoc_base_model.py
+++ generated/python/datadoc_model/datadoc_model/datadoc_base_model.py
@@ -1,7 +1,9 @@
-from pydantic import ConfigDict, BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class DatadocBaseModel(BaseModel):
     """Defines configuration which applies to all Models in this package."""
 
-    model_config = ConfigDict(validate_assignment=True, use_enum_values=True)
+    model_config = ConfigDict(
+        validate_assignment=True, use_enum_values=True, extra="forbid"
+    )
